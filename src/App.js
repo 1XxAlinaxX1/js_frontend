0aa75e803e70d27d0c1de69342d2e77d45f9bbaf
1XxAlinaxX1 <ivanova-dz@mail.ru>
2023-06-19 14:35:08 +0300

    complete server

--- src/App.js
+++ src/App.js
@@ -8,31 +8,30 @@ import { todoAddAll } from './actions';
 
 
 class App extends React.Component {
-	componentDidMount() {
-		fetch('tasks').then(function(res) {
+	componentDidMount(){
+		fetch('tasks').then(function(res){
 			return res.json();
-		}).then((data) => {
+		}).then((data)=>{
 			this.props.dispatch(todoAddAll(data));
 		});
 	}
-
 	
-  render () {
-		 return (
+	render() {
+	  return (
 		<div className="row d-flex justify-content-center container">
 			<div className="col-md-8">
 				<Provider store={this.props.store}>
-					<Router>
-						<Routes>
-							<Route path="/" element={<ToDoList  />} />
-							<Route path="/add" element={<ToDoTaskAdd  />} />
-						</Routes>
-					</Router>
+				<Router>
+					<Routes>
+						<Route path="/" element={<ToDoList />} />
+						<Route path="/add" element={<ToDoTaskAdd />} />
+					</Routes>
+				</Router>
 				</Provider>
 			</div>
 		</div>
-	);
-  }
+	  );
+	}
 }
 
-export default connect() (App);
+export default connect()(App);
--- src/ToDoTask.js
+++ src/ToDoTask.js
@@ -3,77 +3,79 @@ import { connect } from 'react-redux';
 
 import { todoDelete, todoUpdateState } from './actions';
 
+
 class ToDoTask extends React.Component {
-	constructor(props) {
-		super(props); 
+	constructor(props){
+		super(props);
 		
 		this.onStatusClick = this.onStatusClick.bind(this);
 		this.onDeleteClick = this.onDeleteClick.bind(this);
 	}
 	
-	onStatusClick(e) {
+	onStatusClick(e){
 		e.preventDefault();
-		
-	fetch(`tasks/${this.props.task._id}`, {
+
+		fetch(`tasks/${this.props.task._id}`, {
 			method: 'PATCH',
 			body: JSON.stringify({
 				done: !this.props.task.done
 			}),
-			headers:{
+			headers: {
 				'Content-Type': 'application/json'
 			}
-		}).then((res) => {
-			if (res.status === 200) {
+		}).then((res)=>{
+			if(res.status === 200){
 				console.log('Updated');
-			this.props.dispatch(todoUpdateState(this.props.task._id));
+				this.props.dispatch(todoUpdateState(this.props.task._id));
 			}
-			else {
-				console.log('Not Updated');
+			else{
+				console.log('Not updated');
 			}
 		});
-}
-		
+	}
 
-	onDeleteClick(e) {
+	onDeleteClick(e){
 		e.preventDefault();
 		
-		fetch(`task/${this.props.task._id}`, {
+		fetch(`tasks/${this.props.task._id}`,{
 			method: 'DELETE'
-		}).then((res) => {
-			if (res.status === 200) {
+		}).then((res)=>{
+			if(res.status === 200){
 				console.log('Deleted');
 				this.props.dispatch(todoDelete(this.props.task._id));
 			}
-			else {
+			else{
 				console.log('Not deleted');
 			}
 		});
 	}
-  render () {
-		return (
-                  <li className="list-group-item">
-				  {this.props.task.done ? <div className="todo-indicator bg-success"></div> : <div className="todo-indicator bg-focus"></div>}
+
+	render(){
+		return(
+					<li className="list-group-item">
+							  {this.props.task.done ? <div className="todo-indicator bg-success"></div> : <div className="todo-indicator bg-focus"></div>}
                     <div className="widget-content p-0">
                       <div className="widget-content-wrapper">
                         <div className="widget-content-left">
                           <div className="widget-heading">{this.props.task.name}</div>
                           <div className="widget-subheading">
                             <div>{this.props.task.description}</div>
-                            </div>
-                          </div> 
+                          </div>
+                        </div>
                         <div className="widget-content-right">
-                          <button className="border-0 btn-transition btn btn-outline-success" onClick={this.onStatusClick}>
-                            <i className="fa fa-check"></i>
-						</button>
-                          <button className="border-0 btn-transition btn btn-outline-danger" onClick={this.onStatusClick}>
+                          <button className="border-0 btn-transition btn btn-outline-success"onClick={this.onStatusClick}>
+                            <i className="fa fa-check"></i></button>
+                          <button className="border-0 btn-transition btn btn-outline-danger"onClick={this.onDeleteClick}>
                             <i className="fa fa-trash"></i>
+                        
                           </button>
                         </div>
                       </div>
                     </div>
                   </li>
-			 )
-		 }
+			
+			)
+	}
 }
 
-export default connect() (ToDoTask);
+export default connect()(ToDoTask);
